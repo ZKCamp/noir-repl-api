874325c0b928a4b2b2cadd22476d4724c43bdd14
shubham-kanodia <shubhamkanodia123@gmail.com>
2023-06-19 14:26:15 +0530

    Attempt 3

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir -p $HOME/.nargo/bin && \
 curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.5.1/nargo-x86_64-unknown-linux-gnu.tar.gz && \
 tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
